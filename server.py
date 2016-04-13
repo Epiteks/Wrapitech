2a952621dd2fbfe132a2ba829fd9eb70539a9b97
Hugo SCHOCH <hugo.schoch@epitech.eu>
2016-04-13 16:23:45 +0200

    Update server port

--- server.py
+++ server.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
 		app = Flask(__name__)
 		app.register_blueprint(blih.subApp, url_prefix='/blih')
 		app.register_blueprint(intra.subApp, url_prefix='/intra')
-		app.run(port=80, host="0.0.0.0", threaded=True)
+		app.run(port=8080, host="0.0.0.0", threaded=True, debug=True)
 	except Exception as e:
 		print(e)
