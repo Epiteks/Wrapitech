1bb6ee27d0ca12fe7402dad97868e140e0b38158
Hugo SCHOCH <hugo.schoch@epitech.eu>
2016-06-17 16:08:29 +0200

    Update Requester

--- srcs/requester.py
+++ srcs/requester.py
@@ -27,29 +27,70 @@ class	Request(object):
 		self._error = None
 		self._request = None
 		self._cookies = {}
+		self._headers = {"Content-Type": "application/json"}
 
 	def	execute(self):
-		self._done = True
-		self._request = self._types[self._method](self._url, data=self._data, headers={"Content-Type": "application/json"}, cookies=self._cookies)
-		return True if self._request.status_code == 200 else False
+		try:
+			self._done = True
+			self._request = self._types[self._method](self._url, data=self._data, headers=self._headers, cookies=self._cookies)
+			return True if self._request.status_code == 200 else False
+		except Exception as e:
+			raise RequestError(str(e))
 
 	def	parse(self):
 		if self._done and not self._error and self._request.status_code == 200:
 			return {"code": self._request.status_code, "data": self._request.json()}
 		return None
 
+	def getCode(self):
+		if self._done:
+			return self._request.status_code
+		return None
+
+	def getText(self):
+		if self._done:
+			return self._request.text
+		return None
+
 	def	getError(self):
-		return {"code": self._request.status_code, "data": self._request.json()}
+		return {"code": self._request.status_code, "data": self._request.json()} if self._request else None
 
 	def	getCookies(self):
 		return self._request.cookies if self._request else None
 
+	def	getCookie(self, key):
+		try:
+			return self._request.cookies[key] if self._request else None
+		except Exception as e:
+			raise RequestError(str(e))
+
+	def	getHeaders(self):
+		return self._request.headers if self._request else None
+
+	def	getHeader(self, key):
+		try:
+			return self._request.headers[key] if self._request else None
+		except Exception as e:
+			raise RequestError(str(e))
+
 	def	setCookie(self, key, value):
 		self._cookies[key] = value
+
+	def	setHeader(self, key, value):
+		self._headers[key] = value
 
 def executeRequest(req):
 	status = req.execute()
 	return req.parse() if status else req.getError()
+
+def get(url):
+	return executeRequest(Request(url, "GET"))
+
+def post(url, data=None):
+	return executeRequest(Request(url, "POST", data))
+
+def delete(url, data=None):
+	return executeRequest(Request(url, "DELETE", data))
 
 def response(res, code):
 	return Response(response=json.dumps(res), mimetype="application/json", status=code)
